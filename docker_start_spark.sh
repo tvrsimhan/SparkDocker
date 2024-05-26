b8f6606a4413e1e58ba87a36c789905ad3d4168c
Raghavasimhan <Raghavasimhan.TV@tesco.com>
2024-05-27 01:01:09 +0530

    Minor fixes

--- docker_start_spark.sh
+++ docker_start_spark.sh
@@ -5,9 +5,9 @@ if [ -z "$N_WORKERS" ]; then
     N_WORKERS=1
 fi
 
-docker run -dit --name spark-master --network spark-network -p 8081:7077 --entrypoint /bin/bash tvrsimhan27/spark-master
+docker run -dit --name spark-master --network spark-network -p 8081:8080 tvrsimhan27/spark-master
 
 for i in `seq 1 $N_WORKERS`
 do
-    docker run -dit --name "spark-worker${i}" --network spark-network -p "809${i}:7077" -e MEMORY=2G -e CORES=1 tvrsimhan27/spark-worker
+    docker run -dit --name "spark-worker${i}" --network spark-network -p "809${i}:8080" -e MEMORY=2G -e CORES=1 tvrsimhan27/spark-worker
 done
